e05c3372fac42e7251a125f7f31e9c592fb5746e
Carmine Zaccagnino <carmine@PC-Carmine.localdomain>
2017-11-16 17:20:03 +0100

    -

--- tradizionale-subiti.py
+++ tradizionale-subiti.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 from matplotlib import pyplot as plt
-import numpy as np
-import matplotlib
+# import numpy as np
+# import matplotlib
 import pandas as pd
 
-df  = pd.read_csv("dati.csv")
+df = pd.read_csv("dati.csv")
 print (df)
 # Fixing random state for reproducibility
 
